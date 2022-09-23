a28302d51f0591adfcb5f3a98ac739bc2762f550
alfonso314159 <kidalfonso@gmail.com>
2022-09-23 14:51:08 +0800

    check the function of .join()

--- upper_mechanism_run_in_ubuntu/listen_to_main_control/upper_control/src/threading_test.py
+++ upper_mechanism_run_in_ubuntu/listen_to_main_control/upper_control/src/threading_test.py
@@ -1,22 +1,32 @@
 import threading
 from time import sleep
 
-def task():
-    print('task start')
-    sleep(3)
-    print('task end')
+def a(char):
+    if(char == 'a'+ '\n'):
+        print('done'+ '\n')
+    else :
+        print('a fail'+ '\n')
+    for i in range (1,10):
+        sleep(0.5)
+        print(str(i) + 'a'+ '\n')
 
-def count():
-    while(threading.active_count()!= 2):
-        print(str(threading.active_count()))
+def b(char):
+    if(char == 'b'):
+        print('done' + '\n')
+    else :
+        print('b fail')
+    for i in range (1,10):
+        sleep(0.5)
+        print(str(i) + 'b'+ '\n')
 
-t = threading.Thread(target = task)
-c = threading.Thread(target = count)
-print('competition start')
-sleep(3)
-t.start()
-c.start()
-t.join()
-c.join()
-sleep(3)
-print('competition end')
+while True:
+    av = input()
+    a1 = threading.Thread(target = a, args = (av,))
+    b1 = threading.Thread(target = b, args = (av,))
+
+    a1.start()
+    b1.start()
+    a1.join() # 並非卡在這邊等他執行完
+    b1.join()
+    print('finally')
+
